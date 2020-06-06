52fe11ee2211508a92f1421f5a8c595f2ba24fa5
Nishi Agrawal <nishihere19@gmail.com>
2020-06-07 03:03:22 +0530

    added file to manage blog queries

---
+++ core/blogs.js
@@ -0,0 +1,23 @@
+const pool = require('./pool');
+
+function Blog() {};
+
+Blog.prototype={
+    myblogs: function(userid=null, callback){
+        if(userid){
+            let sql=`SELECT * FROM blogs WHERE (id) = ?`
+            pool.query(sql,userid,function(err,result){
+                if(err) throw err
+                if(result.length){
+                    callback(result);
+                }else{
+                    callback(null);
+                }
+            });
+        }
+        else{
+            callback(null);
+        }
+    }
+}
+module.exports= Blog;
